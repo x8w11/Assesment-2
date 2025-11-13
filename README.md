323d35903f1cbca7bfd6f5a0c52923e2e5d46d77
x8w11 <x8w11@students.keele.ac.uk>
2025-11-13 17:03:29 +0000

    Initial commit – added README, .gitignore, and main file placeholder

---
+++ README.md
@@ -0,0 +1,5 @@
+# Snake Escape
+
+A simple Python/Pygame game where you play as the apple trying to escape a chasing snake.
+Use arrow keys to move and avoid being caught.  
+Developed for CSC-44102 Foundations of Programming and Software Engineering (Assessment 2).
---
+++ main.py
@@ -0,0 +1,8 @@
+# I acknowledge the use of ChatGPT (GPT-5, OpenAI, https://chat.openai.com/) to assist in creating this file.
+
+def main():
+    print("Snake Escape - placeholder main file")
+    print("Run 'python main.py' to start (game logic will be added later).")
+
+if __name__ == "__main__":
+    main()
